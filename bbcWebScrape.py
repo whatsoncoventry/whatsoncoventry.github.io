106b6b6db46f08f8608c7acf16df1cc21422eb56
Daniel <32555231+DanielJ0nes@users.noreply.github.com>
2019-02-16 18:51:49 +0000

    Update bbcWebScrape.py

--- bbcWebScrape.py
+++ bbcWebScrape.py
@@ -1,26 +1,16 @@
-"""jonesd37"""
 from bs4 import BeautifulSoup
 import requests
 """BBC Coventry and Warwickshire Scrape"""
-
-
 def getTopStoryBBC():
 	"""Function to get BBC's Coventry and Warwickshire top story. Returns an array of two elements; 0th = title, 1st = contents"""
 	topStory = []
 	response = requests.get('https://www.bbc.co.uk/news/england/coventry_and_warwickshire')
-	soup = BeautifulSoup(response.content, 'html.parser')
-	articleTitles = soup.find_all('span', {'class' : 'title-link__title-text'})
-	articleContents = soup.find_all('p', {'class' : 'skylark__summary'})
-	topStory.append(articleTitles[0].text)
+	siteContent = BeautifulSoup(response.content, 'html.parser')
+	articleTitles = siteContent.find_all('span', {'class' : 'title-link__title-text'}) #Look for specific title 'span' for article title
+	articleContents = siteContent.find_all('p', {'class' : 'skylark__summary'}) #Look for specific summary paragraph for article paragraphs
+	topStory.append(articleTitles[0].text) 
 	topStory.append(articleContents[0].text)
 	return topStory
-
-
 #Example usage
 x = getTopStoryBBC()
 print(' - '.join(x))
-
-
-
-
-
