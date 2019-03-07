2e80e546beacbb0893c41c60ccad8fda9bbad679
Nanofork <47392090+Nanofork@users.noreply.github.com>
2019-03-07 16:45:36 +0000

    Dynamic branch merging
    Moved html files to /views, now considered as 'handlebars' - has placeholder values that the main script fills out when rendering.
    Moved index.js to root folder which is the main script that handles the server, routes, dynamic content.
    Package.json lists all the dependancies for node to get installed at the start of each new build.

---
+++ index.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+'use strict'
+    const Koa = require('koa')
+    const Router = require('koa-router')
+    const stat = require('koa-static')
+    const Database = require('sqlite-async')
+    const handlebars = require('koa-hbs-renderer')
+    
+
+const app = new Koa()
+const router = new Router()
+app.use(handlebars({ paths: { views: `${__dirname}/views` } }))
+app.use(stat('assets'))
+app.use(router.routes())
+
+const PORT = process.env.PORT || 3000;
+const dbName = 'woCoventry.db'
+
+router.get('/', async ctx => {
+	try {
+        let newslettermessage = 'Sign up to our newsletter'
+        if(ctx.query !== undefined && ctx.query.newsletter !== undefined) {
+            const sql = `INSERT INTO newsletter VALUES ("${ctx.query.newsletter}");`
+            newslettermessage = 'Thank you for subscribing'
+            const db = await Database.open(dbName)
+            await db.run(sql)
+            await db.close()
+        }        
+        await ctx.render('index', {message: newslettermessage})
+    } catch(err) {
+		ctx.body = err.message
+	}
+})
+
+router.get('/events', async ctx => {
+	try {
+        let sql = 'SELECT eventname, eventdescription, eventdate, eventtime, eventpostcode, eventaddress FROM eventspage;'
+		let querystring = ''
+		console.log(ctx.query.q)
+		if(ctx.query !== undefined && ctx.query.q !== undefined) {
+			sql = `SELECT eventname, eventdescription, eventdate, eventtime, eventpostcode, eventaddress FROM eventspage 
+							WHERE  upper(eventname) LIKE upper("%${ctx.query.q}%");`
+			querystring = ctx.query.q
+		}
+		const db = await Database.open(dbName)
+		const data = await db.all(sql)
+		await db.close()
+		console.log(data)
+        await ctx.render('search', {events: data, query: querystring})
+        } catch(err) {
+		ctx.body = err.message
+	}
+})
+
+router.get('/elements', async ctx => {
+	try {
+        await ctx.render('elements')
+        } catch(err) {
+		ctx.body = err.message
+	}
+})
+
+router.get('/generic', async ctx => {
+	try {
+        await ctx.render('generic')
+        } catch(err) {
+		ctx.body = err.message
+	}
+})
+
+module.exports = app.listen(PORT, () => console.log(`listening on port ${ PORT }`))
